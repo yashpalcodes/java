44d0bf35840fa0e4173d1ed83f5db4ea8fd05a88
yashpalcodes <rathoreyashpalsingh22@gmail.com>
2025-12-07 19:42:42 +0530

    created first java file

---
+++ first.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class first{
+    public static void sprt(String n){
+        System.out.print(n);
+    }
+    public static void sprtln(String s){
+        System.out.println(s);
+    }
+    public static void iprt(int s){
+        System.out.print(s);
+    }
+    public static void iprtln(int s){
+        System.out.println(s);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("hi this is my first java code.");
+        System.out.print("Enter a no. : ");
+        int n = sc.nextInt();
+        sprt("you have choosen: ");
+        iprtln(n);
+    }
+}
